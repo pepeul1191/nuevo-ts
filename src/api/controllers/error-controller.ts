6ee369d5d4c1772fdb359514efb7f912418c28bb
W4134 <W4134@lab.aca>
2020-01-08 15:54:10 -0500

    error controller muestra error

---
+++ src/api/controllers/error-controller.ts
@@ -0,0 +1,23 @@
+import * as express from 'express'
+import { Request, Response } from 'express'
+import sessionTrueMiddleware from '../middlewares/session-true'
+import IControllerBase from '../interfaces/controller-base'
+
+class ErrorController implements IControllerBase {
+  public path = '/'
+  public router = express.Router()
+
+  constructor() {
+    this.initRoutes()
+  }
+
+  public initRoutes() {
+    this.router.get('/error/access/:number', sessionTrueMiddleware ,this.index)
+  }
+
+  index = (req: Request, res: Response) => {
+    res.status(404).send('Recurso no encontrado');
+  }
+}
+
+export default ErrorController
--- src/configs/bootstrap.ts
+++ src/configs/bootstrap.ts
@@ -1,7 +1,9 @@
 import HomeController from '../api/controllers/home-controller'
 import LoginController from '../api/controllers/login-controller'
+import ErrorController from '../api/controllers/error-controller'
 
 export let controllers = [
   new HomeController(),
   new LoginController(),
+  new ErrorController(),
 ]
